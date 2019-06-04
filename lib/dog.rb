a220ee8dd2d602c0ee85ba9aebe3473747f879e3
Dennis <dennis.basyrov@gmail.com>
2019-06-04 22:01:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog
-  # def initialize(name)
-  #   @name = name
-  # end
+  def initialize(name)
+    @name = name
+  end
   
-   def initialize(breed)
-    @breed = breed
-  end
+  # def initialize(breed)
+  #   @breed = breed
+  # end
  
-  def breed=(breed)
-    @breed = breed
-  end
+  # def breed=(breed)
+  #   @breed = breed
+  # end
  
-  def breed
-    @breed
-  end
+  # def breed
+  #   @breed
+  # end
 end
