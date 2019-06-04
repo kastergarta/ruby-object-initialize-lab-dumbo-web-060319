73fd907e506a35a301179ae41e103dcbf9055ed3
Dennis <dennis.basyrov@gmail.com>
2019-06-04 22:02:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,6 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
-  
-  # def initialize(breed)
-  #   @breed = breed
-  # end
- 
-  # def breed=(breed)
-  #   @breed = breed
-  # end
- 
-  # def breed
-  #   @breed
-  # end
 end
